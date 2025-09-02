c7bd089c06a5dca8289a02f8f02d0fe7657dbf9e
Tomas Gallischek <t.gall@seznam.cz>
2025-09-02 13:44:33 +0200

    autosave

--- shopapp/views.py
+++ shopapp/views.py
@@ -1,3 +1,5 @@
+from multiprocessing import context
+from django.urls import reverse
 from .full_shop import full_shop
 from django.shortcuts import redirect, render
 from hracapp.models import ShopOffer
@@ -6,18 +8,16 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def shop(request):
     print(f"Spuštění funkce renderování obchodu")
+    user = request.user
+    items = ShopOffer.objects.all()
+    items_in_shop = items
+    context = {
+        'user': user,
+        'items_in_shop': items_in_shop,
+    }
 
-    items_in_shop = ShopOffer.objects.all()
-    if items_in_shop == None:
-        vypis = shop_reset(request)
-        context = {
-            'items_in_shop': items_in_shop,
-            'vypis': vypis,
-            'user': request.user,
-        }
-        return render(request, 'shopapp/shop.html', context)
-    else:
-        return render(request, 'shopapp/shop.html')
+    return render(request, 'shopapp/shop.html', context)
+
 
 def shop_reset(request):
     print(f"Spuštění funkce resetování obchodu")
@@ -30,5 +30,5 @@ def shop_reset(request):
         'items_in_shop': items_in_shop,
     }
 
-    return render(request, 'shopapp/shop.html', context)
+    return redirect(reverse('shop-url'))
 
